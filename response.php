95f27f83332e88455dbec548e91f68c400c177a7
JesseHartsuiker <jhartsuiker@roc-dev.com>
2018-12-04 13:50:17 +0100

    12345

--- response.php
+++ response.php
@@ -7,4 +7,19 @@
  */
 
 
-echo $_['wachtwoord'];
+if ($_POST ['voornaam'] ==""){
+    echo "Je moet nog een naam invulllen!";
+    echo "<br><a href=\"formulier.html\">Terug naar het formulier</a>";
+}
+if ($_POST ['adres'] ==""){
+    echo "Je moet nog een adres invulllen!";
+    echo "<br><a href=\"formulier.html\">Terug naar het formulier</a>";
+}
+if ($_POST ['email'] ==""){
+    echo "Je moet nog een email invulllen!";
+    echo "<br><a href=\"formulier.html\">Terug naar het formulier</a>";
+}
+if ($_POST ['wachtwoord'] ==""){
+    echo "Je moet nog een wachtwoord. invulllen!";
+    echo "<br><a href=\"formulier.html\">Terug naar het formulier</a>";
+}
